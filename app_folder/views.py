33a44134464fe31badda7df9352f3d302a5d58af
Michael S DuWors <46774296+MSDPhoenix@users.noreply.github.com>
2021-10-12 04:14:18 +0800

    assignment completed

---
+++ app_folder/views.py
@@ -0,0 +1,17 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render,redirect,HttpResponse
+
+def index1(request):
+    return render(request,'form_page.html')
+
+def index2(request):
+    context = {
+        'name':request.POST['name'],
+        'location':request.POST['location'],
+        'language':request.POST['language'],
+        'comment':request.POST['comment'],
+        'gender':request.POST['gender'],
+        'education':request.POST['education'],
+    }
+    return render(request,'result_page.html',context)
+
